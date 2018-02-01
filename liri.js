e945ccc65b6456569d55697f967942f07bd4a466
JonathonMoore1 <30606521+JonathonMoore1@users.noreply.github.com>
2018-01-31 18:21:02 -0600

    Humble beginnings

--- liri.js
+++ liri.js
@@ -1,5 +1,40 @@
 require("dotenv").config();
+var keys = require("./keys.js");
 
-var spotify = new Spotify(key.spotify);
+// Following two variables return undefined
+var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+var command = process.argv[2];
+var params = process.argv[3];
+
+var request = require("request");
+
+switch (command) {
+
+    case "test":
+        test();
+        break;
+
+    case "twitter":
+        twitter();
+        break;
+}
+
+function test() {
+    console.log("Switch works");
+}
+
+function twitter() {
+    var queryURL = "https://api.twitter.com/1.1/search/tweets.json"
+
+    request(queryURL, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(response);
+        }
+    })
+}
+// Example Request
+// GET https://api.twitter.com/1.1/search/tweets.json?q=%23freebandnames
+// &since_id=24012619984051000&max_id=250126199840518145
+// &result_type=mixed&count=4
